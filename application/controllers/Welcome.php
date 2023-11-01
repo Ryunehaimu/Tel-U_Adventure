9f17845818bdcedee5b8a306f7191d14d67ad8a0
Iqius <iniiqius@gmail.com>
2023-11-01 14:32:58 +0700

    sidebar & masterdat lapor,user

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -21,7 +21,7 @@ class Welcome extends CI_Controller
 	 */
 	public function index()
 	{
-		$this->load->view('header');
-		$this->load->view('./user/landing');
+		// $this->load->view('header');
+		$this->load->view('/admin/MasterData/user/User.html');
 	}
 }
